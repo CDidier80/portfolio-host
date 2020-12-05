d753963ead7cb96663d1a5e0b03171e49037eca0
Luis Rojas <luisrohu91@gmail.com>
2020-12-05 16:34:55 -0700

    update version

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,10 +1,13 @@
 const { User, Profile } = require('../models')
 const { Op, literal, fn, col } = require('sequelize')
-const Helpers = require('../Helpers')
-const log = Helpers.ControllerLoggers.ProfileControllerLog
+// const { Helpers } = require('../Helpers')
+// const { valueIfExists } = Helpers
+
+// console.log('1', valueIfExists(, 'not exist'))
+// const log = Helpers.ControllerLoggers.ProfileControllerLog
 
 const CreateProfile = async (req, res) => {
-    log(this, req)
+    // log(this, req)
     try {
         let userId = parseInt(req.params.user_id)
         let profileBody = {
@@ -15,7 +18,7 @@ const CreateProfile = async (req, res) => {
         let profile = await Profile.create(profileBody)
         res.send(profile)
     } catch (error) {
-        res.status(401).send({ msg: "only one profile"})
+        res.status(401).send({ msg: "only one profile" })
         throw error
     }
 }
