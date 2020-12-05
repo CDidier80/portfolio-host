437dae2aac04b936c198428a6cdc1189569f9652
Lisa Wand <lisavenneker21@gmail.com>
2020-12-05 17:37:37 -0500

    cleaned up and organizing files and paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React, {useEffect, useState } from 'react'
 import { Switch, Route, withRouter } from 'react-router-dom'
-import CreateProfilePage from "./pages/CreateProfilePage"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignupPage from "./pages/SignupPage"
-import ViewProfilesPage from "./pages/ViewProfilesPage"
+import PortfolioPage from "./pages/PortfolioPage"
 // import ProtectedRoute from './components/ProtectedRoute'
-import "./styles/App.css"
 // import Layout from '../src/pages/components/Layout'
 // hello
 
@@ -40,10 +38,9 @@ const App = (props) => {
         ) : (
            <Switch>
               <Route exact path="/" component={(props) =><HomePage {...props} />}/>
-              <Route path="/createprofile" component={(props) => <CreateProfilePage {...props} />}/>
+              <Route path="/portfolio" component={(props) => <PortfolioPage {...props} />}/>
               <Route path="/login" component={(props) => <LoginPage {...props}/>}/>
               <Route path="/signup" component={(props) => <SignupPage {...props}/>}/>
-              <Route path="/profile" component={(props) => <ViewProfilesPage {...props}/>}/>
             </Switch>
         )}
       </main>
