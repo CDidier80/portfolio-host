367680680432c971cfd18b2b2a53b0e41e930699
Collin Didier <collin.didier80@gmail.com>
2020-12-04 18:27:35 -0600

    add middleware, organize import

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
-const app = require('express')()
+const AppRouter = require('./backend/routes/AppRouter')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const helmet = require('helmet')
+const logger = require('morgan')
 
-const AppRouter = require('./backend/routes/AppRouter')
-
+// import express & create a server instance
+const app = require('express')()
 const PORT = process.env.PORT || 5001
 
+//Initialize Middleware
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+app.use(helmet())
+app.use(cors())
+app.disable('X-Powered-By') 
 
+// Test Route
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+// ApiClient.js meets Server.Js at '/api' & sends requests to AppRouter
 app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
