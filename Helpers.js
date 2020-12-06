8d37ddbbab7b1e7fc0e1d922a820a4202368e725
Collin Didier <collin.didier80@gmail.com>
2020-12-05 20:51:50 -0600

    create module of helper functions for logging /debugging

--- Helpers.js
+++ Helpers.js
@@ -1,34 +1,28 @@
-const Helpers = {
-    valueIfExists : (variableToCheck, valueIfNot) => variableToCheck !== "undefined" ? variableToCheck : (valueIfNot !== "undefined" ? valueIfNot : null),
-    ControllerLoggers: {
-        UserControllerLog : (controllerFunction, req) => {
 
-            const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
-            console.log(`Request arrived in UserController.js => controller function: ${controllerFunction} => carrying req.body: `, req.body, paramMessage)
-            return
-        },
-        ProfileControllerLog : (controllerFunction, req) => {
-
-            const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
-            console.log(`Request arrived in ProfileController.js => controller function: ${controllerFunction} => carrying req.body: `, req.body, paramMessage)
-            return
-        },
-
-        ProjectControllerLog : (controllerFunction, req) => {
-
-            const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
-            console.log(`Request arrived in ProjectController.js => controller function: ${controllerFunction} => carrying req.body: `, req.body, paramMessage)
-            return
-        }
+exports.valueIfExists = (variableToCheck, valueIfNot) => variableToCheck ? variableToCheck : valueIfNot
+exports.ControllerLoggers = {
+    UserControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+        // const body
+        const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
+        boolean ? console.log(`Request arrived in UserController.js => controller function: ${controllerFunction} => carrying req.body: `, req.body, paramMessage) : null
+        return
     },
+    ProfileControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+
+        const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
+        boolean ? console.log(`Request arrived in ProfileController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body, paramMessage): null
+        return
+    },
+
+    ProjectControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+
+        const paramMessage = ` and params: ${valueIfExists(req.params, "")}`
+        boolean ? console.log(`Request arrived in ProjectController.js => controller function: ${controllerFunction} => carrying req.body: `, req.body, paramMessage): null
+        return
+    }
 }
 
 
-module.exports = {
-    // ProfileControllerLog, 
-    // ProjectControllerLog, 
-    // UserControllerLog
-    Helpers
-}
-
-// export default module.Helpers
+    // module.exports ={
+    //     ControllerLogs
+    // }
