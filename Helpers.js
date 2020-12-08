2af0ef7245e52705a1cde212b508ad53d69b3be8
Collin Didier <collin.didier80@gmail.com>
2020-12-08 12:59:57 -0600

    add error logging helpers for controllers

--- Helpers.js
+++ Helpers.js
@@ -1,16 +1,29 @@
 exports.valueIfExists = (variableToCheck, valueIfNot) => variableToCheck ? variableToCheck : valueIfNot
+
 exports.ControllerLoggers = {
-    UserControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+    UserControllerLog : (controllerFunction, req, boolean) => {
         boolean ? console.log(`Request arrived in UserController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body,  " and params: ", req.params) : null
         return
     },
-    ProfileControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+    ProfileControllerLog : (controllerFunction, req, boolean) => {
         boolean ? console.log(`Request arrived in ProfileController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body, " and params: ", req.params): null
         return
     },
 
-    ProjectsControllerLog : (controllerFunction, req, valueIfExists, boolean) => {
+    ProjectsControllerLog : (controllerFunction, req, boolean) => {
         boolean ? console.log(`Request arrived in ProjectController.js => controller function: ${controllerFunction.name} => carrying req.body: `, req.body, " and params: ", req.params): null
+        return
+    },
+    UserControllerErrorLog : (controllerFunction, error, boolean) => {
+        boolean ? console.log(`Error caught in UserController.js => controller function: ${controllerFunction.name}: `, error) : null
+        return
+    },
+    ProjectsControllerErrorLog : (controllerFunction, error, boolean) => {
+        boolean ? console.log(`Error caught in ProjectsController.js => controller function: ${controllerFunction.name}: `, error) : null
+        return
+    },
+    ProfileControllerErrorLog : (controllerFunction, error, boolean) => {
+        boolean ? console.log(`Error caught in ProfileController.js => controller function: ${controllerFunction.name}: `, error) : null
         return
     }
 }
