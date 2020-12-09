0bc1b73e61e14ba04c3f21e935387f3687e5c4af
Collin Didier <collin.didier80@gmail.com>
2020-12-09 11:10:01 -0600

    add note

--- client/src/Services/ApiClient.js
+++ client/src/Services/ApiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const ApiClient = axios.create({ baseURL: 'http://localhost:3001/api/' })
 
 
-// used only after user is logged in and authenticated. They maintain authenticated status by checking the validity of the current token
+// used only after user is logged in and authenticated. They maintain authenticated status by checking the validity of the current tokenbuggi
 ApiClient.interceptors.request.use(
     async (config) => {
     const token = localStorage.getItem('token')
