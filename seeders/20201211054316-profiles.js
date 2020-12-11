dd88259935eab25c13efd78449290ce1b2ac3f0c
Luis Rojas <luisrohu91@gmail.com>
2020-12-11 01:01:25 -0700

    seed for users/profiles

--- seeders/20201211054316-profiles.js
+++ seeders/20201211054316-profiles.js
@@ -1,5 +1,5 @@
 'use strict';
-const {User, sequelize } = require('../models')
+const { User, sequelize } = require('../models')
 const faker = require('faker');
 const { query } = require('express');
 
@@ -15,19 +15,93 @@ const profilePictures = [
   'images/pexels-sebastian-libuda-678783.jpg'
 ]
 
+const professional_title = [
+  'Front-End Engineer',
+  'Back-End Engineer',
+  'UX Designer',
+  'Data Scientist',
+  'Full-Stack',
+  'Software Developer',
+  'Software Developer',
+  'Data Analyst',
+  'Dev Ops',
+  'Game Designer',
+]
+
+const locale = [
+  'New Yok',
+  'Boston',
+  'San Francisco',
+  'Seatle',
+  'Denver',
+  'Los Angeles',
+  'Portland',
+  'Atlanta',
+  'Miami',
+  'Detroit',
+]
+
+const organization = [
+  'Google',
+  'Facebook',
+  'Netflix',
+  'Uber',
+  'Amazon',
+  'Netflix',
+  'Amazon',
+  'Tesla',
+  'MomAndPoPShop',
+  'NASA',
+]
+
+
+const bio = [
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+  faker.lorem.sentence(),
+]
+const skills = [
+  'JS',
+  'JAVA',
+  'PYTHON',
+  'C++',
+  'C#',
+  'C',
+  'REACT',
+  'MONGODB',
+  'SQL',
+  'MATLAB',
+]
+
+
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    let users = await User.findAll({ limit: 10 })
     const profiles = await Promise.all(
-      [...Array(10)].map(async () => {
-        let user = await User.findOne({ order: sequelize.random(), raw: true})
+      profilePictures.map(async (picture, index) => {
+        let user = users[index]
+        // let user = await User.findOne({ order: sequelize.random(), raw: true, limit: 10 })
         return {
           user_id: user.id,
-          profile_picture: profilePictures[Math.random * profilePictures.length],
-          professional_title: 'Software Engineer'
+          profile_picture: picture,
+          professional_title: professional_title[index],
+          locale: locale[index],
+          organization: organization[index],
+          bio: bio[index],
+          skills: skills[index]
         }
       })
     )
-    return queryInterface.bulkInsert  ('profiles', profiles)
+    console.log('before return: ', profiles)
+    return queryInterface.bulkInsert('profiles', profiles)
   },
 
   down: async (queryInterface, Sequelize) => {
